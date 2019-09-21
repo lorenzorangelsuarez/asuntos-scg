c0aee0ecde54497cb7499d1b01e5b585e7bb45d5
Lorenzo Rangel Suarez <lorenzorangelsuarez@Lorenzos-MacBook-Pro.local>
2019-09-21 01:50:29 -0500

    Se agregan las funcionalidades para GraphQL y Apollo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExchangeRatesComponent } from './exchange-rates/exchange-rates.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'rates', component: ExchangeRatesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
